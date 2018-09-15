906045360e79e04d010bc807f87217655c1b49dd
BaHui·Qiao <qiaobahuiyouxiang@163.com>
2018-09-15 23:43:55 +0800

    初始化项目

---
+++ BHLogFactoryDemo/BHLogFactory/BHLogFactory.h
@@ -0,0 +1,19 @@
+//
+//  BHLogFactory.h
+//  BHLogFactoryDemo
+//
+//  Created by QiaoBaHui on 2018/9/15.
+//  Copyright © 2018年 QiaoBaHui. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+// NSArray Category
+@interface NSArray (Log)
+
+@end
+
+// NSDictionary Category
+@interface NSDictionary (Log)
+
+@end
---
+++ BHLogFactoryDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BHLogFactoryDemo
+//
+//  Created by QiaoBaHui on 2018/9/15.
+//  Copyright © 2018年 QiaoBaHui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
